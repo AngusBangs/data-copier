1a0dd6c04e7ef8018db72e04f63bbeb51cfc83d0
Angus Bangs <angusjbangs@gmail.com>
2023-02-08 15:21:14 +0000

    dockerised the aplication

--- app/read.py
+++ app/read.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 def get_json_reader(BASE_DIR, table_name, chunksize=1000):
-    file_name=os.listdir(f'{BASE_DIR}\\{table_name}')[0]
+    file_name=os.listdir(f'{BASE_DIR}/{table_name}')[0]
     fp=f'{BASE_DIR}/{table_name}/{file_name}'
     return pd.read_json(fp, lines=True, chunksize=chunksize)
 
